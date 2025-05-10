66fe20303c5ef0591c46bb5c817219fee43dc811
Xavier GIMENO <xavier.gimeno@skillcorner.com>
2025-05-10 06:47:51 +0200

    front fix

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -14,27 +14,23 @@ export type ColorsType = {
 };
 
 export const LightColors: ColorsType = {
-  primary: '#37474F',           // Bleu-gris foncé
+  primary: '#37474F',
   primaryLight: '#62727B',
   primaryDark: '#102027',
-  secondary: '#00BFA5',         // Turquoise
+  secondary: '#00BFA5',
   secondaryLight: '#5df2d6',
   secondaryDark: '#008e76',
-  background: '#f5f5f5',        // Gris très clair
+  background: '#f5f5f5',
   grey100: '#eeeeee',
   grey200: '#e0e0e0',
   grey300: '#bdbdbd',
 };
 
-export const DarkColors: ColorsType = {
-  primary: '#90CAF9',           // Bleu clair
-  primaryLight: '#E3F2FD',
-  primaryDark: '#42A5F5',
-  secondary: '#80CBC4',         // Vert d’eau
-  secondaryLight: '#B2DFDB',
-  secondaryDark: '#4DB6AC',
-  background: '#121212',        // Noir profond
-  grey100: '#1e1e1e',
-  grey200: '#2c2c2c',
-  grey300: '#424242',
+export const DarkColors = {
+  primary: '#00838F',
+  secondary: '#C2185B',
+  background: '#121212',
+  grey100: '#e0e0e0',
+  grey200: '#bdbdbd',
+  grey300: '#9e9e9e',
 };
