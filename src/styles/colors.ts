b2985f95173866cbe2e2d1b3da58b723552a333b
Xavier GIMENO <xavier.gimeno@skillcorner.com>
2025-05-10 06:29:28 +0200

    colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,40 +1,40 @@
- type ColorsType = {
-   background: string;
-   primaryLight: string;
-   primary: string;
-   primaryDark: string;
-   secondaryLight: string;
-   secondary: string;
-   secondaryDark: string;
-   grey100: string;
-   grey200: string;
-   grey300: string;
- };
+// styles/colors.ts
 
-const LightColors: ColorsType = {
-  background: '#1F2028', // If you change background don't forget to change it in white flash handler in public/index.html
-  primaryLight: '#54D6FF',
-  primary: '#54D6FF',
-  primaryDark: '#54D6FF',
-  secondaryLight: '#F93DF1',
-  secondary: '#F93DF1',
-  secondaryDark: '#F93DF1',
-  grey100: '#9F9FA3',
-  grey200: '#71767B',
-  grey300: '#32333B',
+export type ColorsType = {
+  primary: string;
+  primaryLight: string;
+  primaryDark: string;
+  secondary: string;
+  secondaryLight: string;
+  secondaryDark: string;
+  background: string;
+  grey100: string;
+  grey200: string;
+  grey300: string;
 };
 
-const DarkColors: ColorsType = {
-  background: '#1F2028', // If you change background don't forget to change it in white flash handler in public/index.html
-  primaryLight: '#54D6FF',
-  primary: '#54D6FF',
-  primaryDark: '#54D6FF',
-  secondaryLight: '#F93DF1',
-  secondary: '#F93DF1',
-  secondaryDark: '#F93DF1',
-  grey100: '#9F9FA3',
-  grey200: '#71767B',
-  grey300: '#32333B',
+export const LightColors: ColorsType = {
+  primary: '#37474F',           // Bleu-gris foncé
+  primaryLight: '#62727B',
+  primaryDark: '#102027',
+  secondary: '#00BFA5',         // Turquoise
+  secondaryLight: '#5df2d6',
+  secondaryDark: '#008e76',
+  background: '#f5f5f5',        // Gris très clair
+  grey100: '#eeeeee',
+  grey200: '#e0e0e0',
+  grey300: '#bdbdbd',
 };
 
-export { LightColors, DarkColors }; export type { ColorsType };
+export const DarkColors: ColorsType = {
+  primary: '#90CAF9',           // Bleu clair
+  primaryLight: '#E3F2FD',
+  primaryDark: '#42A5F5',
+  secondary: '#80CBC4',         // Vert d’eau
+  secondaryLight: '#B2DFDB',
+  secondaryDark: '#4DB6AC',
+  background: '#121212',        // Noir profond
+  grey100: '#1e1e1e',
+  grey200: '#2c2c2c',
+  grey300: '#424242',
+};
